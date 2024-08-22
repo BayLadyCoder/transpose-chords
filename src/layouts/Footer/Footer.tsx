d76962e3867a9762ace758d47b348cd5867e5036
BayC <rachada_32@hotmail.com>
2024-08-21 22:42:44 -0400

    Remove year

--- src/layouts/Footer/Footer.tsx
+++ src/layouts/Footer/Footer.tsx
@@ -1,19 +1,19 @@
-import { Flex, Text, Icon } from "@chakra-ui/react";
-import { BiCopyright } from "react-icons/bi";
+import { Flex, Text, Icon } from '@chakra-ui/react';
+import { BiCopyright } from 'react-icons/bi';
 
 export const Footer = () => {
   return (
     <Flex
-      justifyContent="center"
-      alignItems="center"
-      position="fixed"
-      bottom="0"
-      w="100%"
-      bg="gray.100"
-      padding="10px 20px"
+      justifyContent='center'
+      alignItems='center'
+      position='fixed'
+      bottom='0'
+      w='100%'
+      bg='gray.100'
+      padding='10px 20px'
     >
-      <Icon as={BiCopyright} h={4} marginRight="5px" />
-      <Text>2024 by Bay Grove</Text>
+      <Icon as={BiCopyright} h={4} marginRight='5px' />
+      <Text>Made by Bay Grove</Text>
     </Flex>
   );
 };
